e358a8e593aa8f1c2ae3cbacd384afa2d31a62ff
xaralien <dimasuzumaki126@gmail.com>
2025-05-09 16:30:12 +0700

    Update Major

--- application/views/parts/admin/Footer.php
+++ application/views/parts/admin/Footer.php
@@ -1,7 +1,7 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2025 &copy; Kodesis.id</p>
+                        <p>Copyright 2025 &copy; Kodesis.id</p>
                     </div>
                     <!-- <div class="float-end">
                         <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
