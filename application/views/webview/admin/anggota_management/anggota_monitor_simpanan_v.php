8651f24960ccb870eddb9d7e58381a6ea38d306e
xaralien <dimasuzumaki126@gmail.com>
2025-08-07 14:25:31 +0700

    Update Monitoring Simpanan Anggota

--- application/views/webview/admin/anggota_management/anggota_monitor_simpanan_v.php
+++ application/views/webview/admin/anggota_management/anggota_monitor_simpanan_v.php
@@ -9,14 +9,14 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Table Monitoring Simpanan Anggota</h3>
+                    <h3>Table Monitoring Iuran Anggota</h3>
                     <!-- <p class="text-subtitle text-muted">Powerful interactive tables with datatables (jQuery required).</p> -->
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="<?= base_url('dashboard') ?>">Dashboard</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Monitoring Simpanan Anggota</li>
+                            <li class="breadcrumb-item active" aria-current="page">Monitoring Iuran Anggota</li>
                         </ol>
                     </nav>
                 </div>
@@ -45,7 +45,7 @@
                                     <th>No</th>
                                     <th>Nomor Anggota</th>
                                     <th>Nama</th>
-                                    <th>Tanggal Simpanan Terakhir</th>
+                                    <th>Tanggal Iuran Terakhir</th>
                                     <th>Status</th>
                                 </tr>
                             </thead>
